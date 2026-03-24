7b84f4244001db4bdfb918926f9b011cbc970e18
ענת שטיבלמן <shtanat5@gmail.com>
2026-03-24 11:19:09 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import AppLayout from './components/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Permits from './pages/Permits';
